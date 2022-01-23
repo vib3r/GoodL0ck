8e3990402d46e6d70d5700e116d03e7cd0d70040
vib3r <96929348+vib3r@users.noreply.github.com>
2022-01-23 17:38:47 +0200

    Update code.py

--- code.py
+++ code.py
@@ -3,11 +3,12 @@
 # [1] Project: GoodL0ck.py
 # [2] Coder: Abdullah, @Vib3r
 # [3] Created: 1/23/2022
+# [4] Updated: 1/23/2022
 # -------------------------
 import random
 import array
 
-MAX_LEN = 50
+MAX_LEN = 50 # Type Here Any Number From [1] to [infinite] to generate Complex Password
 DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']  
 LOCASE_CHARACTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 UPCASE_CHARACTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'M', 'N', 'O', 'p', 'Q','R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z']
